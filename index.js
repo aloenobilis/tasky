ff2fa5cdf958beecc0f760ac9e913f10adfe5c33
Anchorlink <sashnortier@gmail.com>
2018-12-04 11:40:06 +0200

    s4l58 - displaying text on the tray

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const path = require('path'); 
 const electron = require('electron'); 
-const { app, BrowserWindow, Tray } = electron; 
+
 const TimerTray = require('./app/timer_tray.js');                                                  // S4L49
 const MainWindow = require('./app/main_window.js');                                                // S4L56 
+
+const { app, ipcMain } = electron; 
 
 let mainWindow;                                                                                    // S4L38
 let tray;                                                                                          // S4L51
@@ -11,8 +13,12 @@ app.on('ready', () => {
     app.dock.hide();                                                                               // S4L55
     mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);                             // S4L56 - mainWindow.loadURL(`file://${__dirname}/src/index.html`);  
 
-    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';        // S4L40 -  Depending on whether our app is running on Windows or OSX we'll want to use their respetive icons
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';       // S4L40 -  Depending on whether our app is running on Windows or OSX we'll want to use their respetive icons
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`); 
 
-    tray = new TimerTray(iconPath, mainWindow);                                                     // S4L51,  The Tray constructor requires a path for the icon to be dispayed on the status bar 
+    tray = new TimerTray(iconPath, mainWindow);                                                    // S4L51,  The Tray constructor requires a path for the icon to be dispayed on the status bar 
+}); 
+
+ipcMain.on('update-timer', (event, timeLeft) => {                                                  // s4l58
+    tray.setTitle(timeLeft); 
 }); 
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
+import { ipcRenderer } from "electron"; 
 
 import Header from "./Header";
 import TasksIndex from "./TasksIndex";
@@ -42,17 +43,17 @@ class App extends Component {
   // --------------------------------------------------
   // -------- electron event handlers -----------------
   // --------------------------------------------------
-
+  // s4l58
   onAppClose = () => {
 
   };
 
   updateTrayText = title => {
-
+    ipcRenderer.send("update-timer", title); 
   };
 
   timerHasExpired = () => {
-
+    ipcRenderer.send("update-timer", ""); 
   };
 
   // -------- end of electron event handerls ----------
